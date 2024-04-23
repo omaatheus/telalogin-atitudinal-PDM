3efbe648368aa48727d448dd3195bbb6c402d316
Matheuss <124784864+omaatheus@users.noreply.github.com>
2024-04-23 18:19:32 -0300

    alteração no import do ContainerForm

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {
 } from 'react-native';
 import { styles } from '../planets/src/constants/styles.js';
 import { planetinhas } from './src/assets/images.jsx';
-import ContainerForm from './src/components/TopArea/containerform.js';
+import ContainerForm from './src/components/ContainerForm/containerform.js';
 
 export default function App() {
   return (
